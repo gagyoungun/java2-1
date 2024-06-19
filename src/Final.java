1fa323674c00bbbb6e8192e03745e0a92465913d
gagyounggun <gagyounggun@gmail.com>
2024-06-19 15:21:05 +0900

    final0619지창욱

--- src/Final.java
+++ src/Final.java
@@ -12,9 +12,11 @@ public class Final extends JFrame {
     private JTextArea resultArea;
 
     public Final() {
-        ; // [문제2] 타이틀에 '구구단 출력기'라고 나오도록 작성해 주세요.
-        ; // [문제3] 윈도우의 크기를 정의 하세요. 크기는 500 x 400입니다.
-        ; // [문제4] 윈도우 우측 상당의 X아이콘을 클릭하면 프로그램이 종료되도록 합니다.
+        setTitle("구구단 출력기"); // [문제2] 타이틀에 '구구단 출력기'라고 나오도록 작성해 주세요.
+        
+        setSize(500,400); // [문제3] 윈도우의 크기를 정의 하세요. 크기는 500 x 400입니다.
+     
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // [문제4] 윈도우 우측 상당의 X아이콘을 클릭하면 프로그램이 종료되도록 합니다.
         setLocationRelativeTo(null);
 
         // 패널 생성
@@ -33,6 +35,12 @@ public class Final extends JFrame {
     }
 
     private void printGugudan() {
+        for (int i=2; i<=9; i++) {
+            for (int j=1; j<=9; j++) {
+                System.out.println(i+"*"+j+"="+(i*j));
+            }
+            System.out.println(" ");
+        }
         // [문제5] 구구단을 2단에서 9단까지 출력할 수 있도록 반복문을 작성하세요.
         // 각 단마다 빈 줄을 추가해 주세요.
     }
@@ -41,7 +49,7 @@ public class Final extends JFrame {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                ; // [문제6] 결과 화면의 윈도우를 출력해 주세요.
+                new Final(); // [문제6] 결과 화면의 윈도우를 출력해 주세요.
             } 
         });
     }
